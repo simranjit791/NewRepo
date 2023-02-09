8bd74b3a25b6b5a4a7ceb648c1619c280e4f05b1
simranjit791 <simranjit.singh@antiersolutions.com>
2023-02-09 12:35:47 +0530

    first commit

---
+++ src/NewPack/NewClass.java
@@ -0,0 +1,17 @@
+package NewPack;
+
+public class NewClass {
+    public static void main (String[]args)
+    {
+        int a = 4;
+        a=+10 ;
+        System.out.println(a);
+    }
+
+
+
+
+
+
+
+}
